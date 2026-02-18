e347f9e7146cf1d576acb5e4251cd2436bbce3ad
Nabila Nowshin <nabila.nowshin@pathao.com>
2026-02-18 13:16:11 +0600

    get profiles (with age query) and get profile by id

---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param, Query } from '@nestjs/common';
+
+@Controller('profiles')
+export class ProfilesController {
+  // Get /profiles
+
+  //  @Get()
+  //   findAll(): string {
+  //     return 'This action returns all cats';
+  //   }
+
+  @Get()
+  findAll(@Query('age') age: number) {
+    return [{ age }];
+  }
+
+  // Get /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+}
